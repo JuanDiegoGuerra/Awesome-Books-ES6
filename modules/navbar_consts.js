cba8c35eaf299b602eab560e9494fe64dc59eb08
Juan Diego Guerra <jdxelric@gmail.com>
2023-05-01 16:29:24 -0500

    Create navbar_consts.js

---
+++ modules/navbar_consts.js
@@ -0,0 +1,10 @@
+// NAVBAR LINKS
+// List - Link
+export const bookList = document.querySelector('.book-list-container');
+export const listBtn = document.querySelector('.navList');
+export const formContainer = document.querySelector('.form');
+// Add New - Link
+export const addNewBtn = document.querySelector('.navNew');
+// Contact - Link
+export const contactBtn = document.querySelector('.navContact');
+export const contactInfo = document.querySelector('.contact');
